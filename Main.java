2c156b57c2406962c76e742e587614bedc9d0a52
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-05-31 23:19:29 +0200

    selecction started

--- Main.java
+++ Main.java
@@ -8,12 +8,33 @@ public class Main {
 		for(int i = 0; i < prisioners.length; i++){
 			prisioners[i] = new Prisioner();
 		}
-		
-		//------------------------Seleccion--------------------------------
-		
+		//------------------------Selection--------------------------------
+		prisioners = selection(prisioners);
 		//------------------------Crossover--------------------------------
-		
-		//-------------------------Mutación--------------------------------
+		prisioners = crossover(prisioners);
+		//-------------------------Mutation--------------------------------
+		prisioners = mutation(prisioners);
 	}
 
+	private static Prisioner[] selection(Prisioner[] prisioners) {
+		for(int i = 0; i < prisioners.length - 1; i++){
+			for(int j = 0; j < prisioners.length; j++){
+				tournament(prisioners[i], prisioners[j]);
+			}
+		}
+		return prisioners;
+	}
+	
+	private static void tournament(Prisioner prisioner1, Prisioner prisioner2) {
+		
+		
+	}
+
+	private static Prisioner[] crossover(Prisioner[] prisioners) {
+		return prisioners;
+	}
+	
+	private static Prisioner[] mutation(Prisioner[] prisioners) {
+		return prisioners;
+	}
 }
--- Prisioner.java
+++ Prisioner.java
@@ -9,17 +9,29 @@ public class Prisioner {
 		for(int i = 0; i < history.length; i++){
 			history[i] = randomInt(1, 0);
 		}
-		strategy = new int[(int) Math.pow(2, history.length)];
-		for(int j = 0; j < history.length; j++){
+		strategy = new int[(int) Math.pow(2, 2*history.length)];
+		for(int j = 0; j < strategy.length; j++){
 			strategy[j] = randomInt(1, 0);
 		}
 		score = 0;
 	}
 	
+	public void sumScore(int score){
+		this.score += score;
+	}
 	
 	public int[] getHistory() {
 		return history;
 	}	
+	
+	public int[] getStrategy() {
+		return strategy;
+	}
+
+
+	public int getScore() {
+		return score;
+	}
 	
 	private int randomInt(int m, int n){
         return (int) Math.floor(Math.random()*(n-m+1)+m);
